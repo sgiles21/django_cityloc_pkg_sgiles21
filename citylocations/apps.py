7d439fd0134c658b6073fe2ec4b841183108d2c2
Shayla Giles <sngiles561@gmail.com>
2022-04-15 11:17:42 -0400

    publish package

--- citylocations/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CitylocationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'citylocations'
--- citylocations/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def loc_nyc(request):
-    return render(request, 'citylocations/loc_nyc.html', {})
-
